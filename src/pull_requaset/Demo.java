1d1b3ad9f46c99430d20e961c416f0683d7564a2
elkha <elkha@10.0.0.142>
2025-02-04 17:26:57 -0800

    added some method and variable

---
+++ src/pull_requaset/Demo.java
@@ -0,0 +1,20 @@
+package pull_requaset;
+
+public class Demo {
+public static void main(String[] args) {
+	String city []= {"Dublin","SanRamon","Danvil","Concord","Martiniz"};
+	for(String c:city) {
+		System.out.println(c);
+		
+		getNum(2,6,5);
+		getNum(3,5,6);
+		getNum(5,6,4);
+	}
+
+	
+}
+public static void getNum(int a,int b,int c) {
+	System.out.println(a*b-c);
+}
+
+}
