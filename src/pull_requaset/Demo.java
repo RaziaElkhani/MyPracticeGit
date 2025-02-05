240345a6e38d1ea7b8333b4d2ac539629dbb68dc
elkha <elkha@10.0.0.142>
2025-02-05 09:58:51 -0800

    just print

--- src/pull_requaset/Demo.java
+++ src/pull_requaset/Demo.java
@@ -1,34 +1,38 @@
 package pull_requaset;
 
 public class Demo {
-public static void main(String[] args) {
-	String city []= {"Dublin","SanRamon","Danvil","Concord","Martiniz"};
-	for(String c:city) {
-		System.out.println(c);
-		System.out.println("SanRamon");
-		 int num=10;
-		 String name ="Hawa";
-		 
-		getNum(2,6,5);
-		getNum(3,5,6);
-		getNum(5,6,4);
-		
+	public static void main(String[] args) {
+		String city[] = { "Dublin", "SanRamon", "Danvil", "Concord", "Martiniz" };
+		for (String c : city) {
+			System.out.println(c);
+			System.out.println("SanRamon");
+			System.out.println("Dublin");
+			int num = 10;
+
+			String name = "Hawa";
+
+			getNum(2, 6, 5);
+			getNum(3, 5, 6);
+			getNum(5, 6, 4);
+
+		}
+
 	}
 
-	
-}
-public static void getNum(int a,int b,int c) {
-	System.out.println(a*b-c);
-}
-public static int getResult(int num , String country) {
-	int result =0;
-	
-	if (num>0 && country =="USA") {
-		System.out.println("You can get license ");
+	public static void getNum(int a, int b, int c) {
+		System.out.println(a * b - c);
 	}
-	
-	
-	return result;
-}
+
+	public static int getResult(boolean id, String country) {
+		int result = 0;
+
+		if (id == true && country == "USA") {
+			System.out.println("You can get license ");
+		} else {
+			System.out.println("You cannot get license ");
+		}
+
+		return result;
+	}
 
 }
