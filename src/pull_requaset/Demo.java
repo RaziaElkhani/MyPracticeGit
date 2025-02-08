c07c80a2b9fe01e7ae150ccfaf30c5f851b386f3
RaziaElkhani <elkhanirazia@gmail.com>
2025-02-07 16:30:52 -0800

    Update Demo.java

--- src/pull_requaset/Demo.java
+++ src/pull_requaset/Demo.java
@@ -6,7 +6,8 @@ public static void main(String[] args) {
 	for(String c:city) {
 		System.out.println(c);
 		System.out.println("SanRamon");
-		
+		System.out.println("Dublin");
+		System.out.println("Danvil");
 		getNum(2,6,5);
 		getNum(3,5,6);
 		getNum(5,6,4);
